ddb7c7884e372a6c448dea0746975de95a395676
Farrah Yee <classprojectjn@gmail.com>
2018-12-07 02:32:40 +0000

    Day 2

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
-        var techPlacement = "TBD";
+        var id = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
+        var q4Result = $("#question4").val();
+        var totalScore = q1Result + q2Result + q3Result +q4Result;
+        congrats(name, id);
     });
-
 });
+
+function congrats(name, id) {
+    $(".result").text("Congratulations " + name + "! " + "You are " + id + "!");
+}
