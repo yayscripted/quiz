cde601df3325d20dc3b42e9908bf04ee928efc2a
Farrah Yee <classprojectjn@gmail.com>
2018-12-14 02:30:10 +0000

    xmas

--- README.md
+++ README.md
@@ -16,17 +16,17 @@ Make sure to check off your items as you complete them. [ ] => [x]
 ### index.html
 - [x] Update the HTML to reflect your three questions. Expected answers to your new questions should be placeholders in your input box.
 ### JS
-- [ ] Using conditional statements, declare a function for each of your questions that returns a number based on the user's input. 
+- [x] Using conditional statements, declare a function for each of your questions that returns a number based on the user's input. 
 ### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 
 ## Day 2
 ### JS
-- [ ] Use your totalScore variable to store the sum of the result of each function when the user clicks the button.
-- [ ] Write a function that congratulates the user, by name, on their placement and displays the results on the webpage. For now their placement will be "TBD". 
+- [x] Use your totalScore variable to store the sum of the result of each function when the user clicks the button.
+- [x] Write a function that congratulates the user, by name, on their placement and displays the results on the webpage. For now their placement will be "TBD". 
 ### Wrap
-- [ ] Push your changes!
+- [x] Push your changes!
 
 
 ## Day 3
@@ -36,7 +36,7 @@ Make sure to check off your items as you complete them. [ ] => [x]
 - [ ] Push your changes!
 
 ## Extensions
-- [ ] Update the HTML and CSS to reflect your own style.
-- [ ] Change the theme of the quiz.
-- [ ] Add more questions to your quiz.
+- [x] Update the HTML and CSS to reflect your own style.
+- [x] Change the theme of the quiz.
+- [x] Add more questions to your quiz.
 - [ ] Update the function that displays the result so it displays an image in addition to the user-specific placement details.
--- js/script.js
+++ js/script.js
@@ -5,15 +5,60 @@ $(document).ready(function() {
         var name = $("#name").val();
         var id = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
-        var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
-        var q4Result = $("#question4").val();
-        var totalScore = q1Result + q2Result + q3Result +q4Result;
-        congrats(name, id);
-    });
-});
+        var q1Result = $("#nameq").val();
+        var q2Result = $("#dereq").val();
+        var q3Result = $("#mainq").val();
+        var q4Result = $("#powerq").val();
+        var q5Result = $("#genreq").val();
+        
+        function name() {
+            if(q1Result === "Shiro") {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        
+        function dere() {
+            if(q2Result === "Yandere") {
+                return 0;
+            } else if(q2Result === "Tsundere") {
+                return 1;
+            }
+        } 
+        
+        function protag() {
+            if(q3Result === "Yuki") {
+                return 0;
+            } 
+        } 
+        
+        function power() {
+            if(q4Result === "killing") {
+                return 0;
+            }
+        } 
+        
+        function genre() {
+            if(q5Result === "Psychological") {
+                return 0;
+            }
+        }
+        
+        var totalScore = name() + dere() + protag() + power() + genre();
+        
+        function congrats() {
+            if(totalScore >= 0) {
+            var name = $("#question1").val();
+            var id = "Yuno";
+            $(".result").text("Congratulations " + name + "! " + "You are " + id + "!");
+            } else {
+                name = "Ok";
+                id = "cookie"; 
+                $(".result").text("Congratulations " + name + "! " + "You are " + id + "!");
+            }
+            $(".result").text("Congratulations " + name + "! " + "You are " + id + "!");
+        }
+            });
+        });
 
-function congrats(name, id) {
-    $(".result").text("Congratulations " + name + "! " + "You are " + id + "!");
-}
